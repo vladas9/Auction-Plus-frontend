c1bf6b1638a66ce4dc324f957deefd12c92798b9
DdimaPos <postoroncadumitru@gmail.com>
2024-09-05 10:52:25 +0300

    feat: created basic folder/file structures for project

---
+++ src/components/LotItem.jsx
@@ -0,0 +1,13 @@
+//LotItem is the card displayed on homepage
+import React from "react"
+
+export default function LotItem(){
+
+    return(
+        <>
+        <div className="wrapper">
+
+        </div>
+        </>
+    )
+}
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Homepage(){
+    var lots_array=[]
+    return(
+        <>
+            {
+                /*lots_array.map((i, item)=>{
+                    return <LotsItem
+                        lot_number=(item.lot_number)
+                        image_url={item.img_url}
+                        title={item.title}
+                        
+                    />
+                })
+            */}
+        </>
+    )
+}
