ce3f4a7471176c14ccdf4538e25fb50d46e91667
DdimaPos <postoroncadumitru@gmail.com>
2024-09-10 11:53:16 +0300

    feat: created 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Private from './pages/Private'
 import Lot from './pages/Lotpage'
 import Post from './pages/PostlotPage'
 import SuccessPage from "./pages/SuccessPage";
+import Page404 from './pages/Page404'
 
 function App() {
 const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -21,10 +22,11 @@ const [isAuthenticated, setIsAuthenticated] = useState(false);
               <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
               <Route path="/signup" element={<SignUp setIsAuthenticated={setIsAuthenticated} />} />
               <Route path="/profile" element={<Profile />} />
+              <Route path="/profile/postlot" element={<Post />} />
               <Route path="/private" element={<Private/>} />
               <Route path="/lot/:id" element={<Lot />} />
-              <Route path="/profile/postlot" element={<Post />} />
               <Route path="/success" element={<SuccessPage />} />
+              <Route path="*" element={<Page404/>}/>
           </Routes>
       </Router>
   );
