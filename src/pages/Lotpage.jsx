a473885a331d4b46badae2341731c7d9f35f0d40
Мдфв <v.v.titerez@gmail.com>
2024-09-10 14:06:43 +0300

    Done work with popup window

--- src/pages/Lotpage.jsx
+++ src/pages/Lotpage.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
-import styles from "../styles/Lotpage.module.css"
-import LotDisplay from '../components/LotDisplay/LotDisplay'
+import React, { useState, useEffect } from 'react';
+import styles from "../styles/Lotpage.module.css";
+import LotDisplay from '../components/LotDisplay/LotDisplay';
+import LotClosedPopup from "../components/LotClosedPopup/LotClosedPopup";
+
 const Lot = () => {
+
   
+  const [lotEnded, setLotEnded] = useState(false);
+  const winner = { name: "John Doe" };
+  const lot = { max_bid: 100 };
+
+  // Simply to simulate server response by setting lotEnded to true after 5 seconds
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLotEnded(true);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleClosePopup = () => {
+    setLotEnded(false);
+  };
+
   return (
     <div className={styles.lot}>
-      <LotDisplay/>
-    </div>
-  )
-}
+      <LotDisplay />
 
-export default Lot
+      {lotEnded && (
+        <LotClosedPopup winner={winner} lot={lot} onClose={handleClosePopup}/>
+      )}
+    </div>
+  );
+};
+
+export default Lot;
