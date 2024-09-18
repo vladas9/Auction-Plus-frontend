341c48ab7862a9fa7dcfa6f889c857bce1e5e0b8
DdimaPos <postoroncadumitru@gmail.com>
2024-09-18 11:59:22 +0300

    fix: fixed styles on notification page

--- src/pages/NotifPage.jsx
+++ src/pages/NotifPage.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/NotifPage.module.css"
 
 export default function NotificationsPage() {
   const notifications = [
-    { type: "sold", image: "https://nsk.muzmart.com/files/catalog/elektroakustik.jpg", lotId: "41", price: "2000", buyer: "User_1", isNew: true },
+    { type: "sold", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ29bQT_PVcKkA8WTfG4RnqUbT_C8wkGrtnuA&s", lotId: "41", price: "2000", buyer: "User_1", isNew: true },
     { type: "bought", image: "https://nsk.muzmart.com/files/catalog/elektroakustik.jpg", lotId: "13", price: "1500", isNew: true },
     { type: "sold", image: "https://nsk.muzmart.com/files/catalog/elektroakustik.jpg", lotId: "1", price: "2000", buyer: "User_1", isNew: true },
     { type: "bought", image: "https://nsk.muzmart.com/files/catalog/elektroakustik.jpg", lotId: "12", price: "1500", isNew: true },
