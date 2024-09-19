f475bf6158290d0267574becd761a3a2e9caa898
DdimaPos <postoroncadumitru@gmail.com>
2024-09-19 16:16:42 +0300

    feat: started to change styles for tables

--- src/components/PlacedLotsTable/PlacedLotsTable.jsx
+++ src/components/PlacedLotsTable/PlacedLotsTable.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { AgGridReact } from 'ag-grid-react'
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
-
+//import { themeQuartz, iconSetMaterial } from '@ag-grid-community/theming';
+import "./PlacedLotsTable.css"
 import { useState } from 'react';
 import LotImageRenderer from './Renderers/LotImageRenderer';
 import LotStatusRenderer from './Renderers/LotStatusRenderer';
+
+
 export default function PlacedLotsTable({lots}){
     const [colDefs, setColDefs] = useState([
         { 
@@ -35,6 +38,40 @@ export default function PlacedLotsTable({lots}){
             "Top bidder":item.top_bidder_username
         }))
     )
+    // to use myTheme in an application, pass it to the theme grid option
+    /*const myTheme = themeQuartz
+	  .withPart(iconSetMaterial)
+	  .withParams({
+        accentColor: "#087AD1",
+        backgroundColor: "#FFFFFF",
+        borderColor: "#FAFAFA",
+        borderRadius: "6px",
+        browserColorScheme: "light",
+        cellHorizontalPaddingScale: 1.1114285714,
+        chromeBackgroundColor: {
+            ref: "backgroundColor"
+        },
+        columnBorder: false,
+        fontFamily: {
+            googleFont: "Inter"
+        },
+        fontSize: 16,
+        foregroundColor: "#555A60",
+        headerBackgroundColor: "#FFFFFF",
+        headerFontSize: 13,
+        headerFontWeight: 400,
+        headerTextColor: "#84868B",
+        iconSize: "16px",
+        rowBorder: true,
+        rowVerticalPaddingScale: 2,
+        sidePanelBorder: true,
+        spacing: 6,
+        wrapperBorder: false,
+        wrapperBorderRadius: 2
+    });*/
+
+
+
     return(
         <div className="ag-theme-quartz" style={{ height: 500 }}>
             <AgGridReact rowData={rowData} columnDefs={colDefs}/>
