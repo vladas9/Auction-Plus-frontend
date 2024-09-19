b2b93b5457fc06191ef195285e2bd6ec7beee3d5
Мдфв <v.v.titerez@gmail.com>
2024-09-19 13:08:24 +0300

    Done everething in lot page without statistics

--- src/components/LotDisplay/LotDisplay.jsx
+++ src/components/LotDisplay/LotDisplay.jsx
@@ -1,89 +1,70 @@
-import React, { useState } from "react";
-import styles from "./styles.module.css"
-import {Swiper, SwiperSlide} from "swiper/react"
-import "swiper/css"
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import { A11y, Navigation, Pagination} from "swiper/modules";
-export default function LotDisplay(){
-    const [bid_val, setBid_val]=useState(0)
-    var sendBid=()=>{
-        console.log(`send ${bid_val}`)
-    }
-    //example of lot
-    var lot={
-        id: 0,
-        img_srcs: [
-            "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
-            "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
-            "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmCy16nhIbV3pI1qLYHMJKwbH2458oiC9EmA&s",
-        ],
-        title:"Test lot",
-        start_price: 200,
-        last_bid:300,
-        rating: 30,
-        endtime:"12 february" ,
-        status:"closed",
-        description:"loremohegoreoubueoiwbbvnre ierviuwern ui  ifn wfg ic bv cvou hcihv hocvi hofgh oriuhgfo hitrhutbuybeu u osBOP AD OUYBDA PSQREO PIDG H ABTRJ GIUPBH GFPIB DB NPpiub eiieb tih"   
-    }
-    return(
-        <>
-            <div className={styles.display}>
-                <div className={styles.display__top}>
-                    <div className={styles.display__left}>
-                        {/*the image displaying here*/}
-                        <Swiper
-                            modules={[Pagination, Navigation, A11y]}
-                            slidesPerView={1}
-                            navigation
-                            pagination={{ clickable: true }}
-                            
-                        >
-                            <SwiperSlide>
-                                <div className={styles.display__swiper__image} style={{backgroundImage:`url(${lot.img_srcs[0]})`}}></div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className={styles.display__swiper__image} style={{backgroundImage:`url(${lot.img_srcs[1]})`}}></div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className={styles.display__swiper__image} style={{backgroundImage:`url(${lot.img_srcs[2]})`}}></div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className={styles.display__swiper__image} style={{backgroundImage:`url(${lot.img_srcs[3]})`}}></div>
-                            </SwiperSlide>
-                        </Swiper>
+import React, { useState } from 'react';
+import styles from './styles.module.css';
+import ImageSwiper from '../ImageSwiper/ImageSwiper';
+import BidInput from '../BidInput/BidInput';
+import ConfirmButton from '../ConfirmButton/ConfirmButton';
+
+const LotDisplay = (props) => {
+    const [bid, setBid] = useState(''); 
+
+    const handleBidChange = (event) => {
+        setBid(event.target.value);
+    };
+
+    const handleConfirmClick = () => {
+        if ((Number(bid) > props.max_bid) && (props.opened)) {
+            console.log('Bid confirmed:', bid);
+        } else {
+            console.log('Bid is too low or sold.');
+        }
+    };
+
+    const isHigher = (Number(bid) > props.max_bid) && (props.opened);
+
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.main_container}>
+                <div className={styles.up}>
+                    <div className={styles.slider}>
+                        <ImageSwiper img_src={props.img_src}/>
                     </div>
-                    <div className={styles.display__right}>
-                        <div className={styles.display__title}>
-                            {lot.title}
-                        </div>
-                        <div className={styles.display__startPrice}>
-                            Starting price: {lot.start_price}
-                        </div>
-                        <div className={styles.display__input}>
-                            <input value={bid_val} onChange={(e)=>setBid_val(e.target.value)} type="number" />
-                        </div>
-                        <div onClick={sendBid} className={styles.display__confirmButton}>
-                            Confirm
-                        </div>
-                        <div className={styles.display__remainingTime}>
-                            {lot.endtime}
+                    <div className={styles.info_part}>
+                        <div className={styles.title}>{props.title}</div>
+                        <div>
+                            <div className={styles.n_bids}>
+                                <span className="material-symbols-outlined">bar_chart</span>
+                                <p>Number of bids: {props.n_bids}</p>
+                            </div>
+                            <div className={styles.data}>
+                                <span className="material-symbols-outlined">calendar_month</span>
+                                <p>{props.end_date}</p>
+                            </div>
+                            <div className={styles.max_bid}>
+                                <span className="material-symbols-outlined">bid_landscape</span>
+                                <p>Maximum bid: {props.max_bid}$</p>
+                            </div>
+                            <div className={styles.lot_status}>
+                                {props.opened ? <p style={{color:"#29ADB2"}}>Opened</p> : <p style={{color:"#FF3A20"}}>Sold out</p>}
+                            </div>
+                            <div className={styles.to_bid}>
+                            <BidInput bidVal={bid} callback={handleBidChange} />
+                            <ConfirmButton higher={isHigher} callback={handleConfirmClick} />
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className={styles.display__info}>
-                    <div className={styles.info__title}>Description</div>
-                    <div className={styles.info__description}>
-                        {lot.description}
+                <div className={styles.down}>
+                    <div className={styles.intro}><p>Description</p></div> 
+                    <div className={styles.description}>
+                        <p>{props.description.split('\n').map((line, index) => (<React.Fragment key={index}>{line}<br /></React.Fragment>))}</p>
                     </div>
-                    <div className={styles.info__statistics}>
-                        {/*use d3 js or other libs*/}
-                    </div>
+                    <div className={styles.intro}><p>Statistics</p></div> 
+                    
                 </div>
             </div>
-        </>
+        </div>
     )
 }
+
+export default LotDisplay;
--- src/pages/Lotpage.jsx
+++ src/pages/Lotpage.jsx
@@ -8,7 +8,7 @@ const Lot = () => {
   
   const [lotEnded, setLotEnded] = useState(false);
   const winner = { name: "John Doe" };
-  const lot = { max_bid: 100 };
+  const lotwin = { max_bid: 100 };
   
   //fetch the data about the lot
 
@@ -17,7 +17,7 @@ const Lot = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLotEnded(true);
-    }, 5000);
+    }, 12000);
 
     return () => clearTimeout(timer);
   }, []);
@@ -25,13 +25,50 @@ const Lot = () => {
   const handleClosePopup = () => {
     setLotEnded(false);
   };
+
+
+  //Example of info about user
+  var lot={
+    id: 0,
+    img_srcs: [
+        "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
+        "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
+        "https://buffer.com/cdn-cgi/image/w=1000,fit=contain,q=90,f=auto/library/content/images/size/w1200/2023/10/free-images.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmCy16nhIbV3pI1qLYHMJKwbH2458oiC9EmA&s",
+    ],
+    title: "Lot title that is displayed in 2 rows",
+    max_bid: 300,
+    end_date: "2025-09-09T13:35:15+04:00",
+    n_bids: 20,
+    opened: true,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer feugiat commodo metus sed vehicula. \nNulla vitae turpis consectetur, feugiat arcu a, scelerisque metus. \nInteger at magna nisi. Aliquam nec erat semper, semper lacus eget, aliquam erat. Nam quis neque id lorem tristique imperdiet. \nProin viverra sem eleifend, iaculis lacus in, bibendum libero. Suspendisse elementum quis tortor id porta. Phasellus varius nulla non mattis congue. Vestibulum tincidunt in erat ut laoreet.  Nunc iaculis, magna et semper elementum, dolor erat placerat nisi, vel pulvinar augue urna nec sem. Donec rutrum lectus non pellentesque viverra. Aliquam auctor pretium urna, ac faucibus orci porttitor et. \nMauris et dolor venenatis, facilisis turpis sit amet, hendrerit lorem. Integer imperdiet ultricies nisi, in ultricies nulla mattis vitae. Maecenas venenatis posuere cursus. Nulla sollicitudin lobortis aliquet. Mauris a ex aliquam, cursus lectus ac, fringilla ex. Fusce nec sollicitudin enim. Nam egestas sem sit amet nibh bibendum venenatis. Sed tincidunt porttitor ex, sed eleifend velit. Nullam aliquet, tortor id iaculis ultrices, orci felis scelerisque dui, in tristique eros magna in ante. \nCras eu purus quis leo aliquam sollicitudin. Maecenas eu iaculis massa. Curabitur dictum augue vitae scelerisque egestas. In ornare ante id facilisis pulvinar. In a elit enim. Nulla finibus porta lorem volutpat tempor. Donec lacinia lacus sit amet leo interdum blandit pulvinar et nibh. Vestibulum malesuada commodo enim sed convallis. \nSuspendisse lacinia, tellus id luctus venenatis, ante ipsum volutpat eros, sit amet imperdiet nisi lacus id leo. Morbi leo elit, convallis vel varius vel, fringilla a leo. Integer elementum dui et molestie rhoncus.",
+    labels: ["11 January", "12 January", "13 January"],
+    bids_perday: [12, 23, 43],
+    max_bid_perday: [23, 45, 700]
+  }
+
+  //To have good display of end date
+  const dateStr = lot.end_date;
+  const date = new Date(dateStr);
+  const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+  const timeOptions = { hour: '2-digit', minute: '2-digit', hour12: false };
+  const formattedDate = date.toLocaleDateString('en-GB', dateOptions);
+  const formattedTime = date.toLocaleTimeString('en-GB', timeOptions);
+  const result = `${formattedDate} ${formattedTime}`;
+
   
   return (
     <div className={styles.lot}>
-      <LotDisplay />
+      <LotDisplay img_src={lot.img_srcs}
+              description={lot.description}
+              n_bids={lot.n_bids}
+              title={lot.title}
+              end_date={result}
+              max_bid={lot.max_bid}
+              opened={lot.opened}/>
 
       {lotEnded && (
-        <LotClosedPopup winner={winner} lot={lot} onClose={handleClosePopup}/>
+        <LotClosedPopup winner={winner} lot={lotwin} onClose={handleClosePopup}/>
       )}
     </div>
   );
