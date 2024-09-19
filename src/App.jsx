cbf19d8b908f123d1dbd7a5908c14b4fe9639f45
Мдфв <v.v.titerez@gmail.com>
2024-09-19 10:20:41 +0300

    Chaged homepage button and added private_session:id

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,8 @@ import NotificationsPage from "./pages/NotifPage";
 import AdminLotsTable from "./components/AdminComponents/AdminLotsTable";
 import AdminUsersTable from "./components/AdminComponents/AdminUsersTable";
 import { Link } from 'react-router-dom';
+import { matchPath } from 'react-router-dom';
+
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -32,40 +34,42 @@ function App() {
     localStorage.setItem('isAdminAuthenticated', isAdminAuthenticated);
   }, [isAdminAuthenticated]);
 
-  const denied = ['/login', '/signup', '/private-session', '/admin/users', '/admin/lots', '/admin'];
+  const denied = ['/login', '/signup', '/private-session', '/admin/users', '/admin/lots', '/admin', `/private-session/:id`];
 
-
+  const isDeniedPath = denied.some(path => matchPath(path, location.pathname));
+  
   return (
-      <div className='full_container'>
-      {denied.includes(location.pathname) ? 
-        (<Link to='/' className='back_button'><span class="material-symbols-outlined">arrow_left_alt</span></Link>) :
+    <div className='full_container'>
+      {isDeniedPath ? 
+        (<Link to='/' className='back_button'><span className="material-symbols-outlined">arrow_left_alt</span></Link>) :
         (<div className='left_part'><Navbar /></div>)}
-        <div className='right_part'>
-          {denied.includes(location.pathname) ? 
+      <div className='right_part'>
+        {isDeniedPath ? 
           (<></>) :
-            (<Searchbar className="searchbar" isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />)}
-            <Routes>
-              <Route path="/" element={<Homepage />} />
-                <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-                <Route path="/signup" element={<SignUp setIsAuthenticated={setIsAuthenticated} />} />
-              <Route path="/profile" element={<Profile />} />
-                <Route path="/profile/postlot" element={<Post />} />
-              <Route path="/private-session" element={<Private />} />
-                <Route path="/private-session/:id" element={<PrivateSession />} />
-              <Route path="/items" element={<Itemstable/>}/>
-              <Route path="/lot/:id" element={<Lot />} />
-                <Route path="/success" element={<SuccessPage />} />
-              <Route path="/notif" element={<NotificationsPage />} />
-              <Route path="/admin" element={isAdminAuthenticated ? <Admin /> : <Navigate to="/admin/auth" />}>
-                <Route path="lots" element={<AdminLotsTable />} />
-                <Route path="users" element={<AdminUsersTable />} />
-              </Route>
-              <Route path="/admin/auth" element={<AdminAuth setIsAdminAuthenticated={setIsAdminAuthenticated} />} />
-              <Route path="*" element={<Page404 />} />
-            </Routes>
-        </div>
+          (<Searchbar className="searchbar" isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />)}
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+            <Route path="/signup" element={<SignUp setIsAuthenticated={setIsAuthenticated} />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/postlot" element={<Post />} />
+            <Route path="/private-session" element={<Private />} />
+            <Route path="/private-session/:id" element={<PrivateSession />} />
+            <Route path="/items" element={<Itemstable/>}/>
+            <Route path="/lot/:id" element={<Lot />} />
+            <Route path="/success" element={<SuccessPage />} />
+            <Route path="/notif" element={<NotificationsPage />} />
+            <Route path="/admin" element={isAdminAuthenticated ? <Admin /> : <Navigate to="/admin/auth" />}>
+              <Route path="lots" element={<AdminLotsTable />} />
+              <Route path="users" element={<AdminUsersTable />} />
+            </Route>
+            <Route path="/admin/auth" element={<AdminAuth setIsAdminAuthenticated={setIsAdminAuthenticated} />} />
+            <Route path="*" element={<Page404 />} />
+          </Routes>
       </div>
+    </div>
   );
+  
 }
 
 export default App;
