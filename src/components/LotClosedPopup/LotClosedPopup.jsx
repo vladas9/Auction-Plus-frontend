8c021e80673597acc90ac0356e8320c189b9e157
Vladislav <149167046+vvtttvv@users.noreply.github.com>
2024-09-20 16:08:09 +0300

    Feat/lot style/vvtttvv (#52)
    * Navbar and topbar dissapears
    
    * Navbar and search bar are deleted if user is on particular page
    
    * Chaged homepage button
    
    * Chaged homepage button and added private_session:id
    
    * Done everething in lot page without statistics
    
    * Done everething in lot page
    
    * Changed accroding to your request
    
    * Additional changes: restyling  pages - private session code, postlot, created settings page
    
    * fix: removed Opened to be displayed as lot status
    
    ---------
    
    Co-authored-by: DdimaPos <postoroncadumitru@gmail.com>

--- src/components/LotClosedPopup/LotClosedPopup.jsx
+++ src/components/LotClosedPopup/LotClosedPopup.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from 'react-router-dom';
 import styles from "./LotClosedPopup.module.css";
 
-const LotClosedPopup = ({ winner, lot, onClose }) => {
+const LotClosedPopup = ({ winner, lot: max_bid, onClose }) => {
   const navigate = useNavigate();
 
   const handleGoHome = () => {
@@ -14,7 +14,7 @@ const LotClosedPopup = ({ winner, lot, onClose }) => {
       <div className={styles.popupContent}>
         <h2 className={styles.title}>Lot Closed</h2>
         <p className={styles.message}>
-          The winner is <strong>{winner.name}</strong> with a bid of <strong>${lot.max_bid}</strong>
+          The winner is <strong>{winner.name}</strong> with a bid of <strong>${max_bid}</strong>
         </p>
         <div className={styles.buttonGroup}>
           <button onClick={onClose} className={styles.closeBtn}>Close</button>
