a64ad2c7f5b2696a59e05bfb72897a74753e9ef3
DdimaPos <postoroncadumitru@gmail.com>
2024-09-25 14:59:05 +0300

    feat: established post a lot endpoint

--- src/pages/PostlotPage.jsx
+++ src/pages/PostlotPage.jsx
@@ -11,52 +11,92 @@ export default function Post() {
       </>
     )
   }
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("");
-  const [photos, setPhotos] = useState(null);
-  const [terms, setTerms] = useState(false);
-  const [error, setError] = useState("");
   const navigate = useNavigate();
+  const [terms, setTerms]=useState(false);
+  const [error, setError]=useState()
+  const [photos, setPhotos]=useState(null);
+  const [formData, setFormData]=useState({
+    title:"Title from the form",
+    description:"abracadabra description",
+    starting_price:122,
+    category:"furniture",
+    lot_condition:"new",
+    end_date:"2024-09-09T13:34",
+    img_src:[]
+  })
+  var convertImages = (e) =>{
+    var img_src=[];
+    
+    console.log(e.target.files);
+    
+    Array.from(e.target.files).map(value=>{
+      var reader = new FileReader();
+      reader.onloadend = function(){
+        var base64String = reader.result.split(",")[1];
+        img_src.push(base64String);
+      }
+      reader.readAsDataURL(value);
+    })
+    setFormData({...formData, img_src: img_src});
 
-  const handleSubmit = (e) => {
+  }
+
+  var handleChange = (e) =>{
+    const {name, value} = e.target;
+    console.log(name, value);
+    setFormData({...formData, [name]: value});
+    
+  }
+
+  var submit = async (e) => {
     e.preventDefault();
-
-    const selectedDate = new Date(`${date}T${time}`);
-    const currentDate = new Date();
-    if (selectedDate < currentDate) {
-      setError("You cannot select a past date or time.");
-      return;
-    }
-
+    
     if (!terms) {
       setError("You must agree with the conditions to post the auction.");
       return;
-    }
-
-    if (!photos) {
+    }false
+    if (formData.img_src.length<1) {
       setError("You must upload a photo!");
       return;
     }
-
     setError("");
-    console.log({ title, description, price, date, time, photos, terms });
-
-    navigate("/success");
+    postLot();
   };
+  const postLot = async () =>{
+    await fetch("http://localhost:1169/api/post-lot",{
+      method:"POST",
+      headers:{
+        Accept:'application/form-data',
+        "Content-Type":"application-json",
+      },
+      body:{
+        "auth_token":localStorage.getItem("auth-token"),
+        ...formData
+      }
 
+    }).then(res =>{
+      return res.json();
+    }).then(data=>{
+      if(data.posted){
+        alert("your lot is posted now", data.id)
+      }else{
+        throw new Error(data.error_message);
+      }
+    }).catch(err=>{
+      console.error(err.message)
+    })
+  }  
   return (
     <div className={styles.postLotContainer}>
       <h2 className={styles.title}>Place a lot</h2>
-      <form onSubmit={handleSubmit} className={styles.form}>
+      <form onSubmit={submit} className={styles.form}>
         <div className={styles.inputGroup}>
           <input
+            name="title"
             type="text"
             placeholder="Title Input"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            value={formData.title}
+            onChange={handleChange}
             required
             className={styles.input}
           />
@@ -64,20 +104,21 @@ export default function Post() {
 
         <div className={styles.inputGroup}>
           <textarea
+            name="description"
             placeholder="Description Input"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
+            value={formData.description}
+            onChange={handleChange}
             required
-            className={styles.textarea}
           ></textarea>
         </div>
 
         <div className={styles.inputGroup}>
           <input
+            name="starting_price"
             type="number"
             placeholder="Price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
+            value={formData.starting_price}
+            onChange={handleChange}
             required
             className={styles.input}
           />
@@ -86,29 +127,72 @@ export default function Post() {
         <div className={styles.inputGroup}>
           <label className={styles.label}>Choose the date and time</label>
           <input
-            type="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
+            name="end_date"
+            type="datetime-local"
+            min={new Date().toISOString().slice(0,16)}
+            value={formData.end_date}
+            onChange={handleChange}
             required
             className={styles.input}
           />
-          <input
-            type="time"
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
-            required
-            className={styles.input}
-          />
+         
         </div>
+        
+        <div className={styles.selectionGroup}>
+          <div>
+            <label className={styles.label}>Choose the lot condition</label>
+            <div>
+              <input
+                name="lot_condition"
+                type="radio"
+                value="new"
+                id="new"
+                onChange={handleChange}
+                required
+            
+              />
+              <label htmlFor="new">New</label>
+            </div>
+            <div>
+              <input
+                name="lot_condition"
+                type="radio"
+                value="old"
+                id="old"
+                onChange={handleChange}
+                required
+              />
+              <label htmlFor="Old">Old</label>
+            </div>
+          </div>
+          <div className={styles.filterField}>
+          <label>Category</label>
+            <select
+                name="category"
+                value={formData.category}
+                onChange={handleChange}
+                className={styles.filterInput}
+            >
+                <option value="">Select a category</option>
+                <option value="furniture">Furniture</option>
+                <option value="real estate">Real Estate</option>
+                <option value="electronics">Electronics</option>
+                <option value="arts">Arts</option>
+                <option value="others">Others</option>
+            </select>
+          </div>
+        </div>
+        
 
         <div className={styles.inputGroup}>
           <label htmlFor="photos" className={styles.label}>Upload photos</label>
           <input
+            name="photos"
             type="file"
             id="photos"
             checked={photos}
             multiple
-            onChange={(e) => setPhotos(e.target.files)}
+            onChange={convertImages}
             className={styles.input}
           />
         </div>
